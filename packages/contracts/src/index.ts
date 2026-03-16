57d341c341845412e472c25d01554534faf87250
VictorAgahi <victor@agahi.com>
2026-03-16 15:54:25 +0100

    GRPC libs

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,12 @@
+export * from './user/user.pb.js';
+
+export type { RegisterRequest, DeleteUserRequest } from './user/user.command.pb.js';
+export type { LoginRequest, GetUserRequest } from './user/user.query.pb.js';
+export type { EmptyResponse, TokenResponse, UserResponse } from './user/user.responses.pb.js';
+
+export {
+  UserServiceClient,
+  UserServiceController,
+  UserServiceControllerMethods,
+  USER_SERVICE_NAME
+} from './user/user.services.pb.js';
