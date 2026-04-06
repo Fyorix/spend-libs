3d4f0beacbeb3e909ea80969f5bd1016a7be5656
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-04-06 16:41:20 +0200

    file-service : forget to remove an export

--- packages/file-service/src/index.ts
+++ packages/file-service/src/index.ts
@@ -3,4 +3,4 @@ export * from './domain/entities/file.entity.js';
 export * from './domain/core/ports/file.repository.js';
 export * from './infra/mappers/file.mapper.js';
 export * from './infra/models/file.model.js';
-export * from './infra/persistence/file.repository.js';
+
